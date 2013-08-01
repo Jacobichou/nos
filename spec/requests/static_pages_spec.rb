a284fb91f3a4eb59d35be5a7283a5fed9b52346d
Jacob <Jacob@P>
2013-08-01 13:22:16 -0500

    config/application.rb modified for bootstrap pipeline compatibility

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_home_path
-      response.status.should be(200)
-    end
-  end
+
+	describe "Home page" do
+		it "should have the content 'NOS Management System'" do
+			visit static_pages_home_path
+			expect(page).to have_content('NOS Management System')
+		end
+	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit static_pages_help_path
+			expect(page).to have_content('Helpful Information')
+		end
+	end
 end
