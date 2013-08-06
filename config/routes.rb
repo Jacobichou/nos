f5cb181373a1c9b1c01cdb8c3127896a6c8c7e7a
Jacob <Jacob@P>
2013-08-05 23:56:12 -0500

    Navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Nos::Application.routes.draw do
   devise_for :users
   root "static_pages#home"
 
+  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
