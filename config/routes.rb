679f9fd8cd8f1b4fffd6e4235978b2a1c17d9f47
Jacob <Jacob@P>
2013-08-06 11:31:11 -0500

    Tweaking

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Nos::Application.routes.draw do
   devise_for :users
   root "static_pages#home"
 
+  get 'static_pages/index'
+
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
