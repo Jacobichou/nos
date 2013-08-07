110de22b2bc0c05227a50b9aac8cf0f6d8e547bc
Jacob <Jacob@P>
2013-08-06 20:51:39 -0500

    Tweaks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-// = require bootstrap
+//= require bootstrap
 //= require holder
 //= require_tree .
--- app/controllers/project_summary_forms_controller.rb
+++ app/controllers/project_summary_forms_controller.rb
@@ -13,6 +13,20 @@ class ProjectSummaryFormsController < ApplicationController
 		end
 	end
 
+	def update
+	  @psf = Component.find(params[:id])
+
+	  respond_to do |format|
+	    if @psf.update_attributes(psf_params)
+	      format.html { redirect_to @psf, notice: 'PSF was successfully updated.' }
+	      format.json { head :no_content }
+	    else
+	      format.html { render action: "edit" }
+	      format.json { render json: @psf.errors, status: :unprocessable_entity }
+	    end
+	  end
+	end
+
 	private
 		def psf_params
 			params.require(:psf).permit(:project_manager, :location, :budget, :est_revenue, :class)
