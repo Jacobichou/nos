a59aecdc24e185a91afd2df218da4cf324102111
Jacob <Jacob@P>
2013-08-09 16:01:01 -0500

    Get email helper

--- app/helpers/project_summary_forms_helper.rb
+++ app/helpers/project_summary_forms_helper.rb
@@ -2,6 +2,6 @@ module ProjectSummaryFormsHelper
 
 	# get user email for form summary
 	def get_email(psf_item)
-		@email = User.find_by_id(psf_item).email
+		@email = User.find_by_id(psf_item)
 	end
 end
