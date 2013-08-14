2edf04c82523462cb264f0dd5688521617706db1
Jacob <Jacob@P>
2013-08-14 09:56:14 -0500

    email change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Nos::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.default_url_options = { :host => 'nccf-sa.org' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
