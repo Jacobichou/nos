0d2f74deb0b86bae72bdfd7c04e7d6ab0b2dfea4
Jacob <Jacob@P>
2013-08-16 12:40:28 -0500

    deploying

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require classie
-//= require menu
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
