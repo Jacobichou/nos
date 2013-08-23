9f24b7f60d3b9b4cf6ad2064d590190327108832
Jacob <Jacob@P>
2013-08-23 13:33:13 -0500

    Added privileges and modified feed to include owned submissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+# HIERARCHY RANKS
+# 0 = Super Admin
+# 1 = Admin
+# 2 = Supervisor
+# 3 = Employee
+# 4 = Member
+
+
   has_many :project_summary_forms
 
-  	def feed
+  	def feed(user_id)
 	  	@LOWEST_HIERARCHY_LVL = 4
+	  	@user_id = user_id
 	  	if hierarchy > -1
 	  		ProjectSummaryForm.where(hierarchy_lvl: hierarchy..@LOWEST_HIERARCHY_LVL)
 	  	else
 	  		ProjectSummaryForm.where(hierarchy_lvl: hierarchy)
 		end
+		@submitted_psf = ProjectSummaryForm.where(user_id: @user_id)
 	end
 end
